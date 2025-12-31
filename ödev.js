8866409f289ce0a7eedf3194b48ed82f99c9c533
ibrahimekinci20082-collab <ibrahimekinci20082@gmail.com>
2025-12-31 23:01:28 +0300

    Add files via upload

---
+++ ödev.js
@@ -0,0 +1,27 @@
+<script>
+   function Sihirbaz() { 
+   var x = document.getElementById("Kaptan");
+   if (x.style.display === "none") {
+    x.style.display="block";
+   }
+   else{
+
+    x.style.display="none";
+   }
+    
+   }
+    
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+</script>
